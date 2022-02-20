35025d50676048fa3afc8bc3260c8026cf92d36c
Yarkozlov Kozlov <69105791+yarkozloff@users.noreply.github.com>
2022-02-20 23:45:40 +0300

    add Vagrantfile and Start bash

---
+++ start.sh
@@ -0,0 +1,14 @@
+sudo mdadm --zero-superblock --force /dev/sd{b,c,d,e,f};
+sudo mdadm --create --verbose /dev/md0 -l 6 -n 5 /dev/sd{b,c,d,e,f};
+sudo mkdir /etc/mdadm;
+sudo bash -c 'echo "DEVICE partitions" > /etc/mdadm/mdadm.conf';
+sudo mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm/mdadm.conf;
+sudo parted -s /dev/md0 mklabel gpt;
+sudo parted -a optimal /dev/md0 mkpart primary ext4 0% 20%;
+sudo parted -a optimal /dev/md0 mkpart primary ext4 20% 40%;
+sudo parted -a optimal /dev/md0 mkpart primary ext4 40% 60%;
+sudo parted -a optimal /dev/md0 mkpart primary ext4 60% 80%;
+sudo parted -a optimal /dev/md0 mkpart primary ext4 80% 100%;
+for i in $(seq 1 5); do sudo mkfs.ext4 /dev/md0p$i; done;
+sudo mkdir -p /raid/part{1,2,3,4,5};
+for i in $(seq 1 5);do sudo mount /dev/md0p$i /raid/part$i;done;
